015e402c7cfe20e875230adb50e0b353a5cb4f16
Justin TuiSamoa <justintuisamoa@Justins-MacBook-Pro.local>
2021-03-29 17:58:17 -0700

    Implemented Some views

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('anime/', views.animes_index, name='index'),
+  path('anime/<int:anime_id>/', views.animes_detail, name='detail'),
 
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Anime:
+    def __init__(self, title, genre, description, seasons):
+        self.title = title
+        self.genre = genre
+        self.description = description
+        self.seasons = seasons
+
+animes = [
+    Anime('Haikyuu', 'Sports', 'Volleyball Anime', 4),
+    Anime('Jujutsu Kaisen', 'Supernatural/Action', 'Shounen Anime about Curses and Jujustsu Sorcerers', 1),
+    Anime('Black Clover', 'Magical/Action', 'Who will be the Wizard King', 1),
+    Anime('Attack On Titan', 'Science Fiction/Action', 'Humanity is on the brink of extinction', 4),
+
+]
+
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>I Like Turtles</h1>')
 
 def about(request):
     return render(request, 'about.html')
+
+def animes_index(request):
+    animes = Anime.object.all()
+    return render(request, 'animes/index.html', { 'animes': animes })
+
+def animes_detail(request, anime_id):
+    anime = Anime.objects.get(id=anime_id)
+    return render(request, 'animes/detail.html', { 'anime': anime})
